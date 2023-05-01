7494aeb3ce96730d9065a08362a6238cf6e1d8cc
CDJ <casperdjensen@hotmail.com>
2023-05-01 09:11:57 +0200

    ff

--- benchmark_test.py
+++ benchmark_test.py
@@ -14,7 +14,7 @@ from matplotlib.colors import ListedColormap
 import matplotlib.cm as cm
 import timeit
 
-label_video = process_video('images/cheese_gel.avi', threshold=0.3, skip_size=32)
+label_video = process_video('images/cheese_gel.avi', threshold=0.3, skip_size=240)
 
 
 def count_zero_metoden():
@@ -158,7 +158,7 @@ def liste():
 
 def map_fun():
     for label_image in label_video:
-        label_image=label_image[200:700,200:700]
+        # label_image=label_image[200:700,200:700]
         L = 100 # Maximum distances to consider
         M = label_image.shape[0] - L
 
@@ -181,15 +181,51 @@ def map_fun():
         times = []
         for cluster in range(1, curr_image.max()+1):
             ref_cluster = curr_image == cluster
-            rem_clusters = label_image * np.invert(ref_cluster)
+            rem_clusters = np.logical_and(label_image,np.invert(ref_cluster))
             D = ndimage.distance_transform_edt(ref_cluster==0)
             f = np.zeros(L+1)
             g = np.zeros(L+1)
-            
-            K = [D <= x for x in range(L+1)]
-            f = list(map(lambda k: np.count_nonzero(rem_clusters * k), K))
-            g = list(map(lambda k: np.count_nonzero(k), K))
+            for i in range(0,L+1):
+                K = D <= i
+                f[i] = np.count_nonzero(np.logical_and(K,rem_clusters))
+                g[i] = np.count_nonzero(K)            
+            # K = [D <= x for x in range(L+1)]
+            # f = list(map(lambda k: np.count_nonzero(rem_clusters * k), K))
+            # g = list(map(lambda k: np.count_nonzero(k), K))
 
+
+def set_fun():
+    for label_image in label_video:
+        L = 100 # Maximum distances to consider
+        M = label_image.shape[0] - L
+        maxR = 100
+
+        label_image_bounded = np.zeros(label_image.shape, dtype=int)
+        cluster_num = 1
+        for region in regionprops(label_image):
+            minr, minc, maxr, maxc = region.bbox
+            if (minr > L+1) and (minc > L+1) and (maxr < M-1) and (maxc < M-1):
+                pixel_coordinates = region.coords
+                label_image_bounded[pixel_coordinates[:,0],  pixel_coordinates[:,1]] =  cluster_num
+                cluster_num += 1
+
+        bx = (L, M, M, L, L)
+        by = (L, L, M, M, L)
+        curr_image = label_image_bounded
+
+        F = np.zeros(maxR+1)
+        G = np.zeros(maxR+1)
+        for cluster in range(1, curr_image.max()+1):
+            ref_cluster = curr_image == cluster
+            rem_clusters = np.logical_and(label_image,np.invert(ref_cluster))
+            
+            D = ndimage.distance_transform_edt(ref_cluster==0)
+            f = np.zeros(maxR+1)
+            g = np.zeros(maxR+1)
+            for i in range(0,maxR+1):
+                K = D <= i
+                f[i] = np.count_nonzero(np.logical_and(K,rem_clusters))
+                g[i] = np.count_nonzero(K)
 
 
 # loop_metoden()
@@ -200,15 +236,19 @@ def map_fun():
 # execution_time_2 = timeit.timeit(count_zero_metoden, number=20)
 # print("Execution time, count_zero:", execution_time_2)
 
-np_array()
-execution_time = timeit.timeit(np_array, number=15)
-print("Execution time, np_array:", execution_time)
+# np_array()
+# execution_time = timeit.timeit(np_array, number=15)
+# print("Execution time, np_array:", execution_time)
 
-liste()
-execution_time_2 = timeit.timeit(liste, number=15)
-print("Execution time, liste:", execution_time_2)
+# liste()
+# execution_time_2 = timeit.timeit(liste, number=15)
+# print("Execution time, liste:", execution_time_2)
 
 
-map_fun()
-execution_time = timeit.timeit(map_fun, number=15)
+# map_fun()
+# execution_time = timeit.timeit(map_fun, number=15)
+# print("Execution time, map_fun:", execution_time)
+
+# set_fun()
+execution_time = timeit.timeit(map_fun, number=1)
 print("Execution time, map_fun:", execution_time)
