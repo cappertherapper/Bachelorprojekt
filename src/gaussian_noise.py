dcf0061375a394eaa83a357112f59eab933ebd28
CDJ <casperdjensen@hotmail.com>
2023-05-11 08:12:19 +0200

    tertoe

--- src/gaussian_noise.py
+++ src/gaussian_noise.py
@@ -1,7 +1,7 @@
 from skimage import morphology, filters
 from skimage.morphology import label
 import numpy as np
-
+import torch
 
 def generate_noise(size, threshold, disk_size, amount=1):
     """Generates a list of random noise images"""   
@@ -20,3 +20,23 @@ def generate_noise(size, threshold, disk_size, amount=1):
         noise_list.append(labels)
 
     return np.array(noise_list)
+
+def generate_noise_tensor(size, threshold, disk_size, amount=1):
+    """Generates a list of random noise images"""   
+    
+    noise_list = []
+    for i in range(amount):
+        im = torch.randn(size, size)
+
+        disk = morphology.disk(disk_size)
+        disk = torch.from_numpy(disk.astype(float)).to(torch.float32).unsqueeze(0).unsqueeze(0)
+        imFilt = filters.median(torch.unsqueeze(im, 0), disk)
+
+        thresh = threshold if threshold is not None else filters.threshold_otsu(imFilt.numpy())
+        thresh = torch.from_numpy(thresh.astype(float)).to(torch.float32).unsqueeze(0).unsqueeze(0)
+        imThresh = imFilt > thresh
+
+        labels = label(imThresh)
+        noise_list.append(labels)
+
+    return torch.from_numpy(np.array(noise_list)).to(torch.int64)
