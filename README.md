ffb46f1580f11062d281bd26449f1ee75c2374c4
aske0778 <aske.nord.raahauge@gmail.com>
2023-06-18 10:47:32 +0200

    basically done

--- README.md
+++ README.md
@@ -3,6 +3,11 @@ This project provides a novel approach to the study of casein micelle coagulatio
 during milk curdling through the application of spatial analysis methods
 
 
+## Setup
+1. Install dependencies
+2. Import microscopy video of coagulating cheese to path `\images\`
+3. Run notebooks
+
 ## Spacial image analysis
 Run `demo.ipynb` with path to video.
 
--- src/gaussian_noise.py
+++ src/gaussian_noise.py
@@ -25,22 +25,3 @@ def generate_noise_array(size, threshold, smooth, amount=2):
         noise_list.append(label(imThresh))
 
     return np.array(noise_list)
-
-
-# def generate_noise_tensor(size, threshold, disk_size, amount=1):
-#     """Generates a list of random noise images"""   
-    
-#     noise_list = []
-#     for i in range(amount):
-#         im = np.random.normal(size=(size, size))
-
-#         disk = morphology.disk(disk_size)
-#         imFilt = filters.median(im, disk)
-
-#         thresh = threshold if threshold != None else filters.threshold_otsu(imFilt)
-#         imThresh = imFilt > thresh
-
-#         labels = label(imThresh)
-#         noise_list.append(labels)
-
-#     return np.array(noise_list)
